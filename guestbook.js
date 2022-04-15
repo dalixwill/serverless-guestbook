0102942ab9ff1fb918511e0978326073473f818b
Darius Alix-Williams <d.alixwill@gmail.com>
2022-04-15 15:26:30 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://95303251.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
